3641100730d70ff22a650b9e587adb5d8bf21271
sdrs6921 <sdrs6921@naver.com>
2021-03-05 13:35:55 +0900

    init spring project

---
+++ src/main/java/com/codeworrisors/Movie_Community_Web/MovieCommunityWebApplication.java
@@ -0,0 +1,13 @@
+package com.codeworrisors.Movie_Community_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCommunityWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCommunityWebApplication.class, args);
+	}
+
+}
