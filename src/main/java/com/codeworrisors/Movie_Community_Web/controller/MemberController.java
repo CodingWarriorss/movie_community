a266b9e08d8b57d11e8ac299a862e15969b1c51e
yeji an <anyeji1220@gmail.com>
2021-03-09 16:45:42 +0900

    feature : MemberController.java 로그 적용

--- src/main/java/com/codeworrisors/Movie_Community_Web/controller/MemberController.java
+++ src/main/java/com/codeworrisors/Movie_Community_Web/controller/MemberController.java
@@ -4,11 +4,13 @@ import com.codeworrisors.Movie_Community_Web.model.Member;
 import com.codeworrisors.Movie_Community_Web.service.MemberService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/api/members")
 @CrossOrigin("*")
 public class MemberController {
-
+    private final static Logger LOG = Logger.getGlobal();
     private MemberService memberService;
 
     public MemberController(MemberService memberService) {
@@ -23,10 +25,10 @@ public class MemberController {
     public int checkId(@RequestBody Member member){
         int res = memberService.checkId(member.getMemberId());
         if (res == 1){
-            System.out.println("중복체크 : 사용가능한 아이디");
+            LOG.info("중복체크 : 사용가능한 아이디");
         }
         else{
-            System.out.println("중복체크 : 이미 존재하는 아이디");
+            LOG.warning("중복체크 : 이미 존재하는 아이디");
         }
         
         return res;
@@ -41,9 +43,9 @@ public class MemberController {
     public int createMember(@RequestBody Member member){
         int res = memberService.joinMember(member);
         if (res == 1){
-            System.out.println("회원가입 성공");
+            LOG.info("회원가입 : 회원가입 성공");
         } else{
-            System.out.println("이미 존재하는 아이디");
+            LOG.warning("회원가입 : 이미 존재하는 아이디");
         }
 
         return res;
@@ -57,11 +59,11 @@ public class MemberController {
     public int loginMember(@RequestBody Member member){
         int res = memberService.loginMember(member.getMemberId(), member.getMemberPassword());
         if (res == 1){
-            System.out.println("로그인 성공");
+            LOG.info("로그인 : 성공");
         }else if (res == 0){
-            System.out.println("비밀번호 오류");
+            LOG.info("로그인 : 비밀번호 오류");
         }else{
-            System.out.println("존재하지 않는 아이디");
+            LOG.info("로그인 : 존재하지 않는 아이디");
         }
 
         return res;
