3440ca8f1f5539063a1b5012108e997014f93189
yeji an <anyeji1220@gmail.com>
2021-03-09 10:38:27 +0900

    fix: 불필요한 코드 제거

--- src/main/frontend/src/components/signup.component.js
+++ src/main/frontend/src/components/signup.component.js
@@ -34,8 +34,6 @@ export default class SignUp extends Component {
 
     // 아이디 중복체크
     checkID(e) {
-        e.preventDefault();
-
         const data = {
             id: this.state.memberId
         }
@@ -163,7 +161,6 @@ export default class SignUp extends Component {
 
                 <div className="form-group">
                     <div style={{color: 'red'}}>{checkPassword}</div>
-
                 </div>
 
                 <div className="form-group">
