6623cbecd922bffe66f024a31f7c666e25b36396
yeji an <anyeji1220@gmail.com>
2021-03-11 20:38:58 +0900

    fix : allowedOrigins cannot contain the special value "*" 에러 해결

--- src/main/java/com/codeworrisors/Movie_Community_Web/config/CorsConfig.java
+++ src/main/java/com/codeworrisors/Movie_Community_Web/config/CorsConfig.java
@@ -15,12 +15,12 @@ public class CorsConfig {
     public CorsFilter corsFilter(){
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // json 처리 허용
-        config.setAllowedOrigins(Collections.singletonList("*")); // 모든 ip에 응답 허용
+        config.setAllowCredentials(true); // 자바 스크립트 json 처리 허용
+        config.setAllowedOriginPatterns(Collections.singletonList("*")); // 모든 ip에 응답 허용
         config.setAllowedHeaders(Collections.singletonList("*")); // 모든 header에 응답 허용
         config.setAllowedMethods(Collections.singletonList("*")); // 모든 (POST, GET, PUT, POST, DELETE..) 요청에 응답 허용
 
-        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration("/api/**", config);
         return new CorsFilter(source);
     }
 
