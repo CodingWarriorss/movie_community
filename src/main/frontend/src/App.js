dd75a0813244c20eb1fc00b0b3d3d92b92b44a2d
yeji an <anyeji1220@gmail.com>
2021-03-15 14:41:17 +0900

    fix : 부트스트랩 라이브러리 import

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import LoginApp from './component/jwtlogin/LoginApp'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 class App extends Component{
