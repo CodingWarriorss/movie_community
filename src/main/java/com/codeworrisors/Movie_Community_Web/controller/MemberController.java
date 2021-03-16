63cdcfcdc548bb8a8bd86a733c2938196bf834fa
yeji an <anyeji1220@gmail.com>
2021-03-16 14:32:41 +0900

    fix : 아이디 중복체크 메서드 추가, 임시 필드와 메서드 제거

--- src/main/java/com/codeworrisors/Movie_Community_Web/controller/MemberController.java
+++ src/main/java/com/codeworrisors/Movie_Community_Web/controller/MemberController.java
@@ -2,14 +2,12 @@ package com.codeworrisors.Movie_Community_Web.controller;
 
 import com.codeworrisors.Movie_Community_Web.model.Member;
 import com.codeworrisors.Movie_Community_Web.service.MemberService;
-import com.codeworrisors.Movie_Community_Web.service.MemberServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@RequestMapping("/api/members")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class MemberController {
@@ -20,41 +18,27 @@ public class MemberController {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    // test
-    private int id = 0;// 임시
-
-    // test : 성공. 권한 필요 x
-    @GetMapping({"/", ""})
-    public String main() {
-        return "<h1>인덱스 주소요청 테스트</h1>";
+    /*
+    아이디 중복체크: 1 (성공), 0 (이미 존재하는 아이디)
+    */
+    @PostMapping("checkid")
+    public int checkId(@RequestBody Member member) {
+        return memberService.checkId(member.getMemberName());
     }
 
-    // test : 성공. 권한 필요 x
-    @GetMapping("home")
-    public String home() {
-        return "<h1>권한이 '필요없는' 페이지 테스트</h1>";
-    }
-
-    // test : 성공. 권한 필요 o
-    @PostMapping("/api/member/test")
-    public String member() {
-        return "<h1>권한이 '필요한' 페이지 테스트</h1>";
-    }
 
     /*
-    * 회원가입
-    * 중복체크 : 1 (성공), 0 (이미 존재하는 아이디)
-    * */
+     * 회원가입
+     * 한번 더 중복체크: 1 (성공), 0 (이미 존재하는 아이디)
+     * */
     @PostMapping("join")
-    public int createMember(@RequestBody Member member){
-        System.out.println(member);
-        member.setId(id++); // 임시
+    public int createMember(@RequestBody Member member) {
         member.setPassword(bCryptPasswordEncoder.encode(member.getPassword()));
         member.setRole("ROLE_USER");
         int res = memberService.joinMember(member);
-        if (res == 1){
+        if (res == 1) {
             System.out.println("회원가입 성공");
-        } else{
+        } else {
             System.out.println("이미 존재하는 아이디");
         }
 
@@ -62,36 +46,18 @@ public class MemberController {
     }
 
     /*
-    * 로그인
-    * 1 (성공), 0 (비밀번호 틀림), -1 (존재하지 않는 아이디)
-    * */
-//    @PostMapping(value = "/login")
-//    public int loginMember(@RequestBody Member member){
-//        int res = memberService.loginMember(member.getMemberName(), member.getPassword());
-//        if (res == 1){
-//            System.out.println("로그인 성공");
-//        }else if (res == 0){
-//            System.out.println("비밀번호 오류");
-//        }else{
-//            System.out.println("존재하지 않는 아이디");
-//        }
-//
-//        return res;
-//    }
-
-    /*
-    * 회원정보 수정
-    * */
+     * 회원정보 수정
+     * */
     @PutMapping
-    public void updateMember(@RequestBody Member member){
+    public void updateMember(@RequestBody Member member) {
         memberService.updateMember(member);
     }
 
     /*
-    * 회원정보 삭제
-    * */
+     * 회원정보 삭제
+     * */
     @DeleteMapping
-    public void withdrawMember(@RequestBody String id){
+    public void withdrawMember(@RequestBody String id) {
         memberService.withdrawMember(id);
     }
 }
