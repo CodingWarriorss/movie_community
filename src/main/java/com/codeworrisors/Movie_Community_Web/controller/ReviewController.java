4cbdf34cab0e965a477ded1ff2664d1264d18a87
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-03-16 20:33:04 +0900

    feat : 리뷰 컨트롤러 추가

---
+++ src/main/java/com/codeworrisors/Movie_Community_Web/controller/ReviewController.java
@@ -0,0 +1,44 @@
+package com.codeworrisors.Movie_Community_Web.controller;
+
+import com.codeworrisors.Movie_Community_Web.model.Member;
+import com.codeworrisors.Movie_Community_Web.model.Review;
+import com.codeworrisors.Movie_Community_Web.service.ReviewService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/review")
+//@CrossOrigin("*")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    // (임시)
+    Member member = new Member();
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+
+        member.setMemberName("anyeji1220"); // 임시
+    }
+
+    @PostMapping
+    public void createReview(@RequestBody Review review) {
+        // (임시) 실제로는 토큰으로 처리
+        review.setMember(member);
+        reviewService.createReview(review);
+    }
+
+    @PutMapping
+    public void updateReview(@RequestBody Review review) {
+        // (임시) 실제로는 토큰으로 처리
+        review.setMember(member);
+        reviewService.updateReview(review);
+    }
+
+    @DeleteMapping
+    public void deleteReview(@RequestBody Review review) {
+        // (임시) 실제로는 토큰으로 처리
+        review.setMember(member);
+        reviewService.deleteReview(review);
+    }
+}
