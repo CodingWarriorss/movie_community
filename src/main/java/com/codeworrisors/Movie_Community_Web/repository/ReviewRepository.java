ba0987ef39507130543752c46e82ef4b51699c4c
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-03-16 20:25:13 +0900

    feat : 리뷰 리포지토리 인터페이스 추가

--- src/main/java/com/codeworrisors/Movie_Community_Web/repository/ReviewRepository.java
+++ src/main/java/com/codeworrisors/Movie_Community_Web/repository/ReviewRepository.java
@@ -1,2 +1,7 @@
-package com.codeworrisors.Movie_Community_Web.repository;public interface ReviewRepository {
+package com.codeworrisors.Movie_Community_Web.repository;
+
+import com.codeworrisors.Movie_Community_Web.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review,Long> {
 }
