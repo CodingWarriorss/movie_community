84cd99e5e951e48db147dfbe6315d7f628cc9cdb
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-03-17 01:47:47 +0900

    fix : CreateImage 기능 추가

--- src/main/java/com/codeworrisors/Movie_Community_Web/service/ReviewService.java
+++ src/main/java/com/codeworrisors/Movie_Community_Web/service/ReviewService.java
@@ -1,8 +1,10 @@
 package com.codeworrisors.Movie_Community_Web.service;
 
+import com.codeworrisors.Movie_Community_Web.model.Image;
 import com.codeworrisors.Movie_Community_Web.model.Review;
 
 public interface ReviewService {
+    Image createImage(Image image);
     Review createReview(Review review);
     Review updateReview(Review review);
     void deleteReview(Review review);
