0f9e743765de17369ff75cfc01d99c9e488e07ff
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-03-17 12:26:49 +0900

    fix : 테이블 연관관계 with Member, Image 수정. [with Member] Cascade.MERGE (detached entity passed to persist 오류 해결) [with Image] Cascade.ALL (transient 오류 해결)

--- src/main/java/com/codeworrisors/Movie_Community_Web/model/Review.java
+++ src/main/java/com/codeworrisors/Movie_Community_Web/model/Review.java
@@ -24,9 +24,11 @@ public class Review { // 리뷰 게시물
     private Timestamp createDate;   // 작성일
 
     // 작성자
-    @ManyToOne(cascade = CascadeType.ALL) // 여러개의 게시물 - 한명의 작성자
+//    @ManyToOne(cascade = CascadeType.ALL) // [wrong] (여기서 1차 오류 ->  detached entity passed to persist... )
+    @ManyToOne(cascade = CascadeType.MERGE) // 여러개의 게시물 - 한명의 작성자
     private Member member;
     // 이미지
+//    @OneToMany(cascade = CascadeType.MERGE) // [wrong] (여기서 2차 오류 -> transient....)
     @OneToMany(cascade = CascadeType.ALL) // 하나의 리뷰 게시물 - 여러개의 이미지
     private List<Image> imageList;
 
