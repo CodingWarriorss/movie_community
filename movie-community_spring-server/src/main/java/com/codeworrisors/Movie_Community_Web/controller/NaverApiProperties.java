902dba828a32005072b75fda56b57b94c99c4e84
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-03-19 18:11:05 +0900

    feat : naver api 통신 설정값 모음

---
+++ movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/controller/NaverApiProperties.java
@@ -0,0 +1,7 @@
+package com.codeworrisors.Movie_Community_Web.controller;
+
+public interface NaverApiProperties {
+    String CLIENT_ID = "ONSuAEgGuQzbOzI81o4P";
+    String CLIENT_SECRET = "GQJWOGvRe8";
+    String API_URL = "https://openapi.naver.com/v1/search/movie.json?query=";
+}
