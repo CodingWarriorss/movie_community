83111dc27c82ef6f6841d44bf654282cf77e9fbd
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-03-19 18:11:42 +0900

    feat : naver api 서비스 메서드 추가

--- movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/service/ReviewService.java
+++ movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/service/ReviewService.java
@@ -3,7 +3,10 @@ package com.codeworrisors.Movie_Community_Web.service;
 import com.codeworrisors.Movie_Community_Web.model.Image;
 import com.codeworrisors.Movie_Community_Web.model.Review;
 
+import java.io.IOException;
+
 public interface ReviewService {
+    String searchMovie(String title) throws IOException;
     Image createImage(Image image);
     Review createReview(Review review);
     Review updateReview(Review review);
