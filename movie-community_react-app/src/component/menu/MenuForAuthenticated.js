59c296aacd01ffcb371cf509602bcbdf1efaae70
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-03-20 22:24:39 +0900

    fix : 테스트용 프로필 버튼 추가

--- movie-community_react-app/src/component/menu/MenuForAuthenticated.js
+++ movie-community_react-app/src/component/menu/MenuForAuthenticated.js
@@ -11,6 +11,14 @@ const Menu = () => {
             <li className="nav-item">
                 <ReviewWriteBox/>
             </li>
+            {/*테스트용 프로필 버튼*/}
+            <li className="nav-item">
+                <button style={{border: "none", backgroundColor: "white"}}>
+                    <img
+                        src="https://user-images.githubusercontent.com/62331803/111857083-870d3980-8972-11eb-8f75-fa10bb859997.png"
+                        width={40} height={40} style={{borderRadius: 50}}/>
+                </button>
+            </li>
         </ul>
     )
 };
