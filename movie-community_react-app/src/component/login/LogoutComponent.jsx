a1d5940d674aa30b2e79beaa66cdea172b1a3b41
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-03-20 22:24:52 +0900

    fix : 디자인 수정

--- movie-community_react-app/src/component/login/LogoutComponent.jsx
+++ movie-community_react-app/src/component/login/LogoutComponent.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
 import AuthenticationService from "./AuthenticationService.js";
 
-class LogoutComponent extends Component{
+class LogoutComponent extends Component {
 
     render() {
-        return <button onClick={AuthenticationService.logout}>로그아웃</button>
+        return <button className="btn btn-primary" style={{borderColor: "black"}}
+                       onClick={AuthenticationService.logout}>로그아웃</button>
     }
 }
 
