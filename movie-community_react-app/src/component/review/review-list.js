67da71d6e066dabb9a59d46326a8e30b62bb2b60
sdrs6921 <sdrs6921@naver.com>
2021-03-23 19:07:45 +0900

    fix : fix merged file

--- movie-community_react-app/src/component/review/review-list.js
+++ movie-community_react-app/src/component/review/review-list.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 import React, { Component } from "react";
-import SearchbarComponent from "../searchbar/SearchbarComponent";
+import SearchbarComponent from "./searchbar/SearchbarComponent";
 import { Modal, Button, Form } from 'react-bootstrap';
 
 import ReviewBox from './review_box/review-box';
 import dumyData from '../../test_data/review_test_data.json';
-import ReviewBox from "./review_box/review-box";
 
 export default class ReviewList extends Component {
     constructor(props) {
