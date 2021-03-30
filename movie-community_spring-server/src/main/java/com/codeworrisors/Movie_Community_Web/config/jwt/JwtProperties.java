509ee80d584b035addbf4946e4be55fd394c4cbc
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-03-30 21:42:00 +0900

    fix : 테스트를 위한 토큰 유효시간 연장

--- movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/config/jwt/JwtProperties.java
+++ movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/config/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.codeworrisors.Movie_Community_Web.config.jwt;
 
 public interface JwtProperties {
     String SECRET = "cw"; // 서버만 알고 있는 SECRET 키
-    int EXPIRATION_TIME = 60000*10; // 10분
+    int EXPIRATION_TIME = 60000*300; // 10분
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
