d8b4d7dec7faf538b4ea5b03e2bbd52f7c9ab4e0
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-03-30 19:10:52 +0900

    fix : LIKE 리포지토리 리팩토링 - like 개수 반환을 위한 count 변수 추가

--- movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/repository/LikeRepository.java
+++ movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/repository/LikeRepository.java
@@ -1,14 +1,11 @@
 package com.codeworrisors.Movie_Community_Web.repository;
 
 import com.codeworrisors.Movie_Community_Web.model.Likes;
-import com.codeworrisors.Movie_Community_Web.model.Member;
-import com.codeworrisors.Movie_Community_Web.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Likes, Long> {
-    Optional<Likes> findByMemberAndReview(Member member, Review review);
-
-    void deleteById(Long id);
+    Optional<Likes> findByMemberIdAndReviewId(Long memberId, Long reviewId);
+    int countByReviewId(long reviewId);
 }
