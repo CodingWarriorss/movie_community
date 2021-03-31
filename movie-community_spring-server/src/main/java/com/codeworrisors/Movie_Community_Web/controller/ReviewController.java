55d94d6c5caa38e1b74b2e6ec74c833069774188
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-03-31 18:16:22 +0900

    fix : like, comment 관련 메서드 Review 내로 이동.

--- movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/controller/ReviewController.java
+++ movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/controller/ReviewController.java
@@ -1,7 +1,9 @@
 package com.codeworrisors.Movie_Community_Web.controller;
 
-import com.codeworrisors.Movie_Community_Web.config.auth.PrincipalDetails;
+import com.codeworrisors.Movie_Community_Web.security.auth.PrincipalDetails;
+import com.codeworrisors.Movie_Community_Web.dto.CreateCommentDto;
 import com.codeworrisors.Movie_Community_Web.dto.CreateReviewDto;
+import com.codeworrisors.Movie_Community_Web.dto.UpdateCommentDto;
 import com.codeworrisors.Movie_Community_Web.dto.UpdateReviewDto;
 import com.codeworrisors.Movie_Community_Web.model.Review;
 import com.codeworrisors.Movie_Community_Web.service.ReviewService;
@@ -12,24 +14,35 @@ import org.springframework.data.domain.Sort;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.io.*;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping(value = "/api/review")
 public class ReviewController {
-    static final String RESULT = "result";
-    static final String REVIEW = "review";
     static final String SUCCESS = "SUCCESS";
     static final String FAIL = "FAIL";
 
-    private final static int PAGE_SIZE = 5;
+    static final String RESULT = "result";
+    static final String REVIEW = "review";
+    static final String COMMENT = "comment";
+    static final int PAGE_SIZE = 5;
 
     private final ReviewService reviewService;
+//    private final CommentService commentService;
+//    private final LikeService likeService;
 
-    public ReviewController(ReviewService reviewService) {
+    public ReviewController(ReviewService reviewService
+//            ,
+//                            CommentService commentService,
+//                            LikeService likeService
+    ) {
         this.reviewService = reviewService;
+//        this.commentService = commentService;
+//        this.likeService = likeService;
     }
 
     /*
@@ -114,6 +127,109 @@ public class ReviewController {
             response.put(RESULT, FAIL + "/권한 없는 리뷰에 대한 삭제 요청");
         } catch (NoSuchElementException e) {
             response.put(RESULT, FAIL + "/존재하지 않는 리뷰에 대한 삭제 요청");
+        }
+
+        return response;
+    }
+
+
+    /*
+     * 댓글 생성
+     * response(성공/실패 여부, 만들어진 commentId)
+     * */
+    @PostMapping("/comment")
+    public JSONObject postComment(@AuthenticationPrincipal PrincipalDetails userDetail,
+                                  @RequestBody CreateCommentDto createCommentDto) {
+        JSONObject response = new JSONObject();
+
+        try {
+            response.put(RESULT, SUCCESS);
+            response.put(COMMENT, reviewService.createComment(userDetail.getMember(), createCommentDto));
+        } catch (EntityNotFoundException e) {
+            response.put(RESULT, FAIL + "/존재하지 않는 리뷰에 대한 댓글 추가 요청");
+        }
+
+        return response;
+    }
+
+    /*
+     * 댓글 수정 => POSTMAN에서만 해봄
+     * response(성공/실패 여부)
+     * */
+    @PutMapping("/comment")
+    public JSONObject modifyComment(@AuthenticationPrincipal PrincipalDetails userDetail,
+                                    UpdateCommentDto updateCommentDto) {
+        JSONObject response = new JSONObject();
+
+        try {
+            response.put(RESULT, SUCCESS);
+            reviewService.updateComment(userDetail.getMember(), updateCommentDto);
+        } catch (IllegalStateException e) {
+            response.put(RESULT, FAIL + "/권한 없는 댓글에 대한 수정 요청");
+        } catch (NoSuchElementException e) {
+            response.put(RESULT, FAIL + "/존재하지 않는 댓글에 대한 수정 요청");
+        }
+
+        return response;
+    }
+
+    /*
+     * 댓글 삭제 => POSTMAN에서만 해봄
+     * response(성공/실패 여부)
+     * */
+    @DeleteMapping("/comment")
+    public JSONObject deleteComment(@AuthenticationPrincipal PrincipalDetails userDetail,
+                                    @RequestParam long commentId) {
+        JSONObject response = new JSONObject();
+
+        try {
+            response.put(RESULT, SUCCESS);
+            reviewService.deleteComment(userDetail.getMember(), commentId);
+        } catch (IllegalStateException e) {
+            response.put(RESULT, FAIL + "/권한 없는 댓글에 대한 삭제 요청");
+        } catch (NoSuchElementException e) {
+            response.put(RESULT, FAIL + "/존재하지 않는 댓글에 대한 삭제 요청");
+        }
+
+        return response;
+    }
+
+
+    /*
+     * 좋아요 추가/삭제
+     * response(LIKE/UNLIKE 여부)
+     * */
+    @PostMapping("/like")
+    public JSONObject likeReview(@AuthenticationPrincipal PrincipalDetails userDetail,
+                                 @RequestBody Map<String, Long> params) {
+        JSONObject response = new JSONObject();
+
+        try {
+            response.put(RESULT, SUCCESS);
+            reviewService.createLike(userDetail.getMember(), params.get("reviewId"));
+            response.put("status", "LIKE");
+        } catch (IllegalStateException e) {
+            response.put(RESULT, FAIL + "/이미 LIKE 상태");
+        } catch (NoSuchElementException e) {
+            response.put(RESULT, FAIL + "/존재하지 않는 리뷰에 대한 LIKE 요청");
+        }
+
+        return response;
+    }
+
+    @DeleteMapping("/like")
+    public JSONObject unlikeReview(@AuthenticationPrincipal PrincipalDetails userDetail,
+                                   @RequestParam("reviewId") long reviewId) {
+        JSONObject response = new JSONObject();
+
+        try {
+            response.put(RESULT, SUCCESS);
+            reviewService.deleteLike(userDetail.getMember(), reviewId);
+            response.put("status", "UNLIKE");
+        } catch (IllegalStateException e) {
+            response.put(RESULT, FAIL + "/이미 UNLIKE 상태");
+        } catch (NoSuchElementException e) {
+            response.put(RESULT, FAIL + "/존재하지 않는 리뷰에 대한 UNLIKE 요청");
         }
 
         return response;
--- movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/service/ReviewService.java
+++ movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/service/ReviewService.java
@@ -1,35 +1,22 @@
 package com.codeworrisors.Movie_Community_Web.service;
 
-import com.codeworrisors.Movie_Community_Web.constant.NaverApiProperties;
+import com.codeworrisors.Movie_Community_Web.property.StaticResourceProperties;
+import com.codeworrisors.Movie_Community_Web.dto.CreateCommentDto;
 import com.codeworrisors.Movie_Community_Web.dto.CreateReviewDto;
+import com.codeworrisors.Movie_Community_Web.dto.UpdateCommentDto;
 import com.codeworrisors.Movie_Community_Web.dto.UpdateReviewDto;
-import com.codeworrisors.Movie_Community_Web.model.Image;
-import com.codeworrisors.Movie_Community_Web.model.Member;
-import com.codeworrisors.Movie_Community_Web.model.Review;
-import com.codeworrisors.Movie_Community_Web.repository.ImageRepository;
-import com.codeworrisors.Movie_Community_Web.repository.LikeRepository;
-import com.codeworrisors.Movie_Community_Web.repository.MemberRepository;
-import com.codeworrisors.Movie_Community_Web.repository.ReviewRepository;
-import com.codeworrisors.Movie_Community_Web.request.NaverAPIResponse;
-import com.codeworrisors.Movie_Community_Web.request.NaverMovie;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.codeworrisors.Movie_Community_Web.model.*;
+import com.codeworrisors.Movie_Community_Web.repository.*;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -39,18 +26,17 @@ import java.util.*;
 @Transactional
 public class ReviewService {
 
-    @Value("${file.path}")
-    private String fileRealPath; // 이미지 저장 경로
-
     private final ReviewRepository reviewRepository;
     private final ImageRepository imageRepository;
     private final LikeRepository likeRepository;
+    private final CommentRepository commentRepository;
     private final MemberRepository memberRepository;
 
-    public ReviewService(ReviewRepository reviewRepository, ImageRepository imageRepository, LikeRepository likeRepository, MemberRepository memberRepository) {
+    public ReviewService(ReviewRepository reviewRepository, ImageRepository imageRepository, LikeRepository likeRepository, CommentRepository commentRepository, MemberRepository memberRepository) {
         this.reviewRepository = reviewRepository;
         this.imageRepository = imageRepository;
         this.likeRepository = likeRepository;
+        this.commentRepository = commentRepository;
         this.memberRepository = memberRepository;
     }
 
@@ -61,8 +47,8 @@ public class ReviewService {
     public List<Review> getAll(Pageable pageable) {
         List<Review> reviews = reviewRepository.findAll(pageable).getContent();
         reviews.forEach(review -> {
-                    review.setLikeCount(likeRepository.countByReviewId(review.getId()));
-                });
+            review.setLikeCount(likeRepository.countByReviewId(review.getId()));
+        });
 
         return reviews;
     }
@@ -137,7 +123,7 @@ public class ReviewService {
     private String saveFile(MultipartFile file) throws IOException {
         UUID uuid = UUID.randomUUID(); // 식별자 생성
         String uuidFilename = uuid + "_" + file.getOriginalFilename();
-        Path filePath = Paths.get(fileRealPath + uuidFilename);
+        Path filePath = Paths.get(StaticResourceProperties.IMAGE_UPLOAD_PATH + uuidFilename);
         Files.write(filePath, file.getBytes());
         return uuidFilename;
     }
@@ -174,7 +160,7 @@ public class ReviewService {
 
     private void removeFile(String uuidFilename) {
         try {
-            Path filePath = Paths.get(fileRealPath + uuidFilename);
+            Path filePath = Paths.get(StaticResourceProperties.IMAGE_UPLOAD_PATH + uuidFilename);
             Files.delete(filePath);
         } catch (IOException e) {
             System.out.println("이미지 파일 삭제 오류");
@@ -183,72 +169,68 @@ public class ReviewService {
 
 
     /*
-     * Naver 영화 API 통신
-     */
-    public String searchMovie(String title) throws IOException {
-        Map<String, String> requestHeaders = new HashMap<>();
-        requestHeaders.put("X-Naver-Client-Id", NaverApiProperties.CLIENT_ID);
-        requestHeaders.put("X-Naver-Client-Secret", NaverApiProperties.CLIENT_SECRET);
-        return get(NaverApiProperties.API_URL + title, requestHeaders);
+     * CREATE, UPDATE, DELETE COMMENT
+     * */
+    public JSONObject createComment(Member member, CreateCommentDto createCommentDto) throws EntityNotFoundException {
+        Comments saved = commentRepository.save(
+                new Comments(createCommentDto.getContent(), member, reviewRepository.getOne(createCommentDto.getReviewId())));
+
+        JSONObject result = new JSONObject();
+        result.put("commentId", saved.getId());
+        return result;
     }
 
-    private String get(String apiUrl, Map<String, String> requestHeaders) throws IOException {
-        // 요청
-        HttpURLConnection conn = connect(apiUrl);
-        try {
-            conn.setRequestMethod("GET");
-            for (Map.Entry<String, String> header : requestHeaders.entrySet()) {
-                conn.setRequestProperty(header.getKey(), header.getValue());
-            }
+    public JSONObject updateComment(Member member, UpdateCommentDto updateCommentDto) throws IllegalStateException, NoSuchElementException {
+        JSONObject result = new JSONObject();
 
-            // 응답
-            int responseCode = conn.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+        commentRepository.findById(updateCommentDto.getCommentId())
+                .ifPresentOrElse(comment -> {
+                    if (member.getId() != comment.getMember().getId())
+                        throw new IllegalStateException("권한 없는 댓글에 대한 수정 요청");
+                    comment.setContent(updateCommentDto.getContent());
+                }, () -> {
+                    throw new NoSuchElementException("존재하지 않는 댓글에 대한 수정 요청");
+                });
 
-                return readBody(conn.getInputStream());
-            } else { // 에러 발생
-                return readBody(conn.getErrorStream());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("API 요청과 응답 실패", e);
-        } finally {
-            conn.disconnect();
-        }
+        return result;
     }
 
-    private HttpURLConnection connect(String apiUrl) {
-        try {
-            URL url = new URL(apiUrl);
-            return (HttpURLConnection) url.openConnection();
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
-        } catch (IOException e) {
-            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
-        }
+    public void deleteComment(Member member, long commentId) throws IllegalStateException, NoSuchElementException {
+        commentRepository.findById(commentId)
+                .ifPresentOrElse(comment -> {
+                    if (member.getId() != comment.getMember().getId())
+                        throw new IllegalStateException("권한 없는 댓글에 대한 삭제 요청");
+                    commentRepository.delete(comment);
+                }, () -> {
+                    throw new NoSuchElementException("존재하지 않는 댓글에 대한 삭제 요청");
+                });
     }
 
-    Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private String readBody(InputStream body) {
-        InputStreamReader streamReader = new InputStreamReader(body);
+    /*
+     * CREATE, DELETE LIKE
+     * */
+    public void createLike(Member member, long reviewId) throws IllegalStateException, NoSuchElementException {
+        if (reviewRepository.findById(reviewId).isEmpty())
+            throw new NoSuchElementException("존재하지 않는 리뷰");
 
-        ObjectMapper jsonParser = new ObjectMapper();
+        likeRepository.findByMemberIdAndReviewId(member.getId(), reviewId)
+                .ifPresentOrElse(like -> {
+                    throw new IllegalStateException("이미 like한 상태");
+                }, () -> {
+                    likeRepository.save(new Likes(member, reviewRepository.getOne(reviewId)));
+                });
+    }
 
-        //Jaskson 사용 Deserializing? 후 sorting 다시 serializing
-        try {
-            NaverAPIResponse naverAPIResponse = jsonParser.readValue(body, NaverAPIResponse.class);
-            naverAPIResponse.getItems().sort(new Comparator<NaverMovie>() {
-                @Override
-                public int compare(NaverMovie o1, NaverMovie o2) {
-                    double o1Rating = Double.parseDouble(o1.getUserRating());
-                    double o2Rating = Double.parseDouble(o2.getUserRating());
-                    return (o1Rating > o2Rating) ? -1 : 1;
-                }
-            });
+    public void deleteLike(Member member, long reviewId) throws IllegalStateException, NoSuchElementException {
+        if (reviewRepository.findById(reviewId).isEmpty())
+            throw new NoSuchElementException("존재하지 않는 리뷰");
 
-            return jsonParser.writeValueAsString(naverAPIResponse);
-        } catch (IOException e) {
-            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
-        }
+        likeRepository.findByMemberIdAndReviewId(member.getId(), reviewId)
+                .ifPresentOrElse(like -> {
+                    likeRepository.delete(like);
+                }, () -> {
+                    throw new IllegalStateException("이미 unlike 상태");
+                });
     }
 }
