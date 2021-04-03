9ad6b514d92321cfdc8b3c54c24dddd6c0e641f7
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-04-04 02:56:06 +0900

    feat : 기본 아바타 이미지 url 세팅

--- movie-community_react-app/src/component/constants/APIConstants.js
+++ movie-community_react-app/src/component/constants/APIConstants.js
@@ -1,3 +1,4 @@
 
+export const DEFAULT_AVATAR_URL = "https://user-images.githubusercontent.com/62331803/113481597-5e8a5100-94d5-11eb-82f0-7fc907f7e477.png";
 export const REST_API_SERVER_URL = "http://localhost:8080";
 export const IMAGE_RESOURCE_URL = REST_API_SERVER_URL + "/resource/image/";
