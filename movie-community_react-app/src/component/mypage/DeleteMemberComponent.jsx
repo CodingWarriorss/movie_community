e6056e5ede3e983946e7f3625f5fbbb8f25e2d5a
yeji an <62331803+Ahnyezi@users.noreply.github.com>
2021-04-05 14:52:04 +0900

    feat : 회원 삭제 모달창

---
+++ movie-community_react-app/src/component/mypage/DeleteMemberComponent.jsx
@@ -0,0 +1,102 @@
+import React, {Component} from 'react'
+import {Modal, Button} from "react-bootstrap";
+import axios from "axios";
+import {REST_API_SERVER_URL} from "../constants/APIConstants";
+import AuthenticationService from "../login/AuthenticationService";
+
+class DeleteMemberComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            close: false,
+            password: ''
+        }
+
+        this.handleClose = this.handleClose.bind(this);
+        this.withdraw = this.withdraw.bind(this);
+        this.isWithdrawPossible = this.isWithdrawPossible.bind(this);
+    }
+
+    isWithdrawPossible() {
+        const {password} = this.state
+        const memberName = localStorage.getItem('authenticatedMember');
+
+        axios.post(REST_API_SERVER_URL + '/login', {memberName, password})
+            .then(response => {
+                if (response.headers['authorization'].startsWith('Bearer')) {
+                    this.withdraw();
+                } else {
+                    alert('비밀번호가 틀렸습니다.');
+                }
+            })
+    }
+
+    withdraw() {
+        const config = {
+            headers: {
+                'Authorization': localStorage.getItem('token')
+            }
+        }
+
+        axios.delete(REST_API_SERVER_URL + '/api/member', config)
+            .then(function (response) {
+                console.log(response.data);
+                if (response.data === 1) {
+                    alert('회원 정보가 삭제되었습니다.');
+                    AuthenticationService.logout();
+                } else {
+                    alert('비밀번호가 틀렸습니다.');
+                }
+            })
+    }
+
+    changeHandler = (e) => {
+        this.setState(
+            {
+                [e.target.name]: e.target.value
+            }
+        )
+    }
+
+    handleClose() {
+        this.setState({
+            close: true
+        });
+    }
+
+    render() {
+        const {
+            isOpen, close,
+        } = this.props;
+
+        return (
+            <Modal
+                size="xs"
+                show={isOpen}
+                onHide={close}>
+                <Modal.Header closeButton>회원 탈퇴</Modal.Header>
+
+                <Modal.Body>
+                    <div className="form-group">
+                        <label>비밀번호</label>
+                        <input type="password" className="form-control" placeholder="비밀번호"
+                               name="password" value={this.state.password} onChange={this.changeHandler}/>
+                    </div>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={close}>
+                        취소
+                    </Button>
+                    <Button variant="danger" onClick={this.isWithdrawPossible}>
+                        탈퇴
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        )
+    }
+}
+
+export default DeleteMemberComponent
