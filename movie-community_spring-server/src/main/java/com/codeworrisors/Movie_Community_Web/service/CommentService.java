ef1ef37a19e4e86f839397693d7cff9481d8da39
sdrs6921 <sdrs6921@naver.com>
2021-04-06 18:07:09 +0900

    fix : 댓글 추가하기 ReviewService -> CommentService

--- movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/service/CommentService.java
+++ movie-community_spring-server/src/main/java/com/codeworrisors/Movie_Community_Web/service/CommentService.java
@@ -1,4 +1,44 @@
 package com.codeworrisors.Movie_Community_Web.service;
 
+import com.codeworrisors.Movie_Community_Web.dto.CreateCommentDto;
+import com.codeworrisors.Movie_Community_Web.model.Comments;
+import com.codeworrisors.Movie_Community_Web.model.Member;
+import com.codeworrisors.Movie_Community_Web.repository.CommentRepository;
+import com.codeworrisors.Movie_Community_Web.repository.ReviewRepository;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
 public class CommentService {
+    private CommentRepository commentRepository;
+    private ReviewRepository reviewRepository;
+
+    public CommentService(CommentRepository commentRepository, ReviewRepository reviewRepository) {
+        this.commentRepository = commentRepository;
+        this.reviewRepository = reviewRepository;
+    }
+
+    public JSONObject createComment(Member member, CreateCommentDto createCommentDto)
+            throws EntityNotFoundException{
+        JSONObject result = new JSONObject();
+
+        reviewRepository.findById(createCommentDto.getReviewId())
+                .orElseThrow(() -> {
+                    throw new EntityNotFoundException();
+                });
+
+        Comments savedComments =
+                new Comments(
+                        createCommentDto.getContent(),
+                        member,
+                        reviewRepository.getOne(createCommentDto.getReviewId())
+                );
+
+        result.put("commentId", savedComments.getId());
+        return result;
+    }
+
+    public \
 }
