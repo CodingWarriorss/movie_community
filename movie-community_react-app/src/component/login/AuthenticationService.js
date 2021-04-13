952ad7c1fa8e10cb93b925ef4f40e3eb1380440e
sdrs6921 <sdrs6921@naver.com>
2021-04-13 15:59:19 +0900

    fix : debug

--- movie-community_react-app/src/component/login/AuthenticationService.js
+++ movie-community_react-app/src/component/login/AuthenticationService.js
@@ -44,6 +44,7 @@ class AuthenticationService {
 
         axios.get(requestURL, config)
             .then((response) => {
+                console.log(response.data);
                 const member = response.data.member;
                 localStorage.setItem('profileImg', member.profileImg ? IMAGE_RESOURCE_URL + member.profileImg : DEFAULT_AVATAR_URL);
                 window.location.replace('/');
