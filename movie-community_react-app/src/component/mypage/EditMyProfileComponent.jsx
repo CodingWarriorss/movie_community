ccb78e42049355f1d51e0905bb4a4d3118d22353
sdrs6921 <sdrs6921@naver.com>
2021-04-14 15:00:41 +0900

    fix : fix merge complict

--- movie-community_react-app/src/component/mypage/EditMyProfileComponent.jsx
+++ movie-community_react-app/src/component/mypage/EditMyProfileComponent.jsx
@@ -1,181 +1,183 @@
 import React, {Component} from 'react'
-import '../css/Profile.css';
-import EditMyProfileComponent from "./EditMyProfileComponent";
-import {DEFAULT_AVATAR_URL, IMAGE_RESOURCE_URL, REST_API_SERVER_URL} from "../constants/APIConstants";
+import {Modal, Button, Form} from "react-bootstrap";
 import axios from "axios";
-import ProfileReviewComponent from "./ProfileReviewComponent";
-import DeleteMemberComponent from "./DeleteMemberComponent";
+import {DEFAULT_AVATAR_URL, IMAGE_RESOURCE_URL, REST_API_SERVER_URL} from "../constants/APIConstants";
+import ImageUploader from "react-images-upload";
 
-class MyProfileComponent extends Component {
-
+class EditMyProfileComponent extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            isModalEditOpen: false,
-            isModalWithdrawOpen: false,
+            close: false,
 
-            member: {
-                memberName: '',
-                name: '',
-                website: '',
-                bio: '',
-                profileImg: '',
-            },
+            name: '',
+            email: '',
+            website: '',
+            bio: '',
 
-            reviewList: [],
+            picture: [],
         }
 
         this.loadMemberInfo = this.loadMemberInfo.bind(this);
-        this.modifyMemberInfo = this.modifyMemberInfo.bind(this);
-        this.loadReview = this.loadReview.bind(this);
-        this.openEditModal = this.openEditModal.bind(this);
-        this.closeEditModal = this.closeEditModal.bind(this);
-        this.openWithdrawModal = this.openWithdrawModal.bind(this);
-        this.closeWithdrawModal = this.closeWithdrawModal.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleSummit = this.handleSummit.bind(this);
+        this.onDrop = this.onDrop.bind(this);
     }
 
     componentDidMount() {
-        const memberName = localStorage.getItem('authenticatedMember');
-        this.loadMemberInfo(memberName);
-        this.loadReview(memberName);
+        this.loadMemberInfo();
     }
 
-    openEditModal() {
-        this.setState({isModalEditOpen: true});
-    }
-
-    closeEditModal() {
-        this.setState({isModalEditOpen: false});
-    }
-
-    openWithdrawModal() {
-        this.setState({isModalWithdrawOpen: true});
-    }
-
-    closeWithdrawModal() {
-        this.setState({isModalWithdrawOpen: false});
-    }
-
-    loadMemberInfo(memberName) {
+    loadMemberInfo() {
         const requestUrl = REST_API_SERVER_URL + '/api/member';
         let config = {
             headers: {
                 'Authorization': localStorage.getItem('token')
             },
             params: {
-                memberName: memberName
+                memberName: localStorage.getItem('authenticatedMember')
             }
         }
         axios.get(requestUrl, config)
             .then((response) => {
                 const member = response.data.member;
-                console.log("response : " + response.data);
                 this.setState({
-                    member: {
-                        memberName: member.memberName,
-                        name: member.name,
-                        website: member.website,
-                        bio: member.bio,
-                        profileImg: member.profileImg ? IMAGE_RESOURCE_URL + member.profileImg : DEFAULT_AVATAR_URL,
-                    },
+                    name: member.name,
+                    website: member.website,
+                    email: member.email,
+                    bio: member.bio,
+                    profileImg: member.profileImg ? IMAGE_RESOURCE_URL + member.profileImg : DEFAULT_AVATAR_URL,
                 });
             }).catch((error) => {
 
         })
     }
 
-    modifyMemberInfo(member) {
-        this.setState({
-            member: {
-                name: member.name,
-                website: member.website,
-                bio: member.bio,
-                profileImg: member.profileImg ? IMAGE_RESOURCE_URL + member.profileImg : DEFAULT_AVATAR_URL,
-            },
-        })
+    changeHandler = (e) => {
+        this.setState(
+            {
+                [e.target.name]: e.target.value
+            }
+        )
     }
 
-    loadReview(memberName) {
-        const requestUrl = REST_API_SERVER_URL + '/api/review';
-        let config = {
+    handleClose() {
+        this.setState({
+            close: true
+        });
+    }
+
+    handleSummit() {
+        const {
+            name, email, website, bio, picture
+        } = this.state;
+
+        if (email
+            && !/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(email)) {
+            alert('올바르지 않은 이메일 형식입니다.');
+            return;
+        }
+
+        const url = REST_API_SERVER_URL + '/api/member';
+
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('email', email);
+        formData.append('bio', bio);
+        formData.append('website', website);
+        if (picture.length > 0) {
+            formData.append('profileImg', picture[0]);
+        }
+
+        const config = {
             headers: {
-                'Authorization': localStorage.getItem('token')
-            },
-            params: {
-                memberName: memberName,
-                pageIndex: 0
+                'Authorization': localStorage.getItem('token'),
+                'Content-Type': "multipart/form-data"
             }
         }
-        axios.get(requestUrl, config)
-            .then((response) => {
-                this.setState({
-                    reviewList: response.data
-                });
-            }).catch((error) => {
 
-        })
+        axios.put(url, formData, config)
+            .then((response) => {
+                const data = response.data;
+                if (data.result === 'success') {
+                    const member = data.member;
+                    this.props.modifyMemberInfo(member);
+                    alert('수정이 완료되었습니다.');
+                    this.props.close();
+                    // window.location.replace('/mypage');
+                } else {
+                    alert('수정에 실패했습니다.');
+                }
+            });
+    }
+
+    onDrop(pictureFile) {
+        this.setState({
+            picture: this.state.picture.concat(pictureFile)
+        });
     }
 
     render() {
         const {
-            isModalEditOpen, isModalWithdrawOpen, reviewList
-        } = this.state;
+            isOpen, close,
+        } = this.props;
 
         const {
-            memberName, name, website, bio, profileImg
-        } = this.state.member;
+            name, email, website, bio
+        } = this.state;
 
         return (
-            <main id="profile" style={{backgroundColor: "white", padding: "100px 100px"}}>
-                <header className="profile__header">
-                    <div className="avatar__container">
-                        <form action="" id="frm_profile_img">
-                            <input type="file" name="profileImage" style={{display: "none"}}/>
-                        </form>
-                        <img
-                            src={profileImg}
-                            id="profile_image"
-                            style={{cursor: "pointer"}}/>
-                    </div>
+            <Modal
+                size="xs"
+                show={isOpen}
+                onHide={close}>
+                <Modal.Header closeButton>회원 정보 수정</Modal.Header>
 
-                    <div className="profile__info">
-                        <div className="profile__title">
-                            <h1>{memberName}</h1>
-                            <a href="#">
-                                <button className="profile_edit_btn" onClick={this.openEditModal}>
-                                    회원수정
-                                </button>
-                                <EditMyProfileComponent member={this.state.member}
-                                                        modifyMemberInfo={this.modifyMemberInfo}
-                                                        isOpen={isModalEditOpen} close={this.closeEditModal}/>
-                            </a>
-                            <a href="#">
-                                <button className="profile_follow_btn" onClick={this.openWithdrawModal}>
-                                    회원탈퇴
-                                </button>
-                                <DeleteMemberComponent isOpen={isModalWithdrawOpen}
-                                                       close={this.closeWithdrawModal}/>
-                            </a>
-                        </div>
+                <Modal.Body>
+                    <ImageUploader
+                        singleImage={true}
+                        withIcon={false}
+                        withPreview={true}
+                        onChange={this.onDrop}
+                        buttonText="프로필 이미지 변경"
+                        imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+                        maxFileSize={5242880}
+                    />
 
-                        <div className="profile__bio" style={{paddingLeft: "3px"}}>
-                            <p className="profile__fullname">{name}</p>
-                            <p>{bio}</p>
-                            <p><a href={website}>{website}</a></p>
-                        </div>
+                    <div className="form-group">
+                        <label>이름</label>
+                        <input type="text" className="form-control" placeholder="ex. 안무비"
+                               name="name" value={name} onChange={this.changeHandler}/>
                     </div>
-                </header>
-                <div className="profile__photo-grid">
-                    {reviewList.map(
-                        reviewData => {
-                            return <ProfileReviewComponent reviewData={reviewData} key={reviewData.reviewId}/>
-                        }
-                    )}
-                </div>
-            </main>
+                    <div className="form-group">
+                        <label>이메일</label>
+                        <input type="email" className="form-control" placeholder="ex. movie520@gmail.com"
+                               name="email" value={email} onChange={this.changeHandler}/>
+                    </div>
+                    <div className="form-group">
+                        <label>웹사이트</label>
+                        <input type="text" className="form-control" placeholder="ex. https://www.ilovemovie.com"
+                               name="website" value={website} onChange={this.changeHandler}/>
+                    </div>
+                    <div className="form-group">
+                        <label>자기소개</label>
+                        <textarea className="form-control" placeholder="ex. 저는 만화영화를 사랑하는 안무비입니다. 무야호오오"
+                                  name="bio" value={bio} onChange={this.changeHandler}/>
+                    </div>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={close}>
+                        취소
+                    </Button>
+                    <Button variant="primary" onClick={this.handleSummit}>
+                        등록
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         )
     }
 }
 
-export default MyProfileComponent
+export default EditMyProfileComponent
